19fe2c61f5d4363f8258319c82fa7c853186b32f
Blackdiamond1996 <blackdiamond26041996@gmail.com>
2022-10-19 02:38:10 -0700

    jj

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Main - Prints _putchar, followed by a new line
+ * main - Prints _putchar, followed by a new line
  * by Blackdiamond
  * Return: Always 0 (Success)
  */
